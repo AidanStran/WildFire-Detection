b102b12a7d78e82f182ede4fa3f039a4de89c838
Aidan Stranaghan <Aidanstranaghan@gmail.com>
2024-11-02 16:53:24 -0400

    New things

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 
 model = YOLO("yolo11n-cls.pt")
-results = model.train(data="mnist160", epochs=100, imgsz=64)
+results = model.train(data="config.yaml", epochs=1, imgsz=64)
 
+print("hello")
