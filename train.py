fabefb5214236a7687059344b375f040df48b653
Aidan Stranaghan <Aidanstranaghan@gmail.com>
2024-11-06 19:14:05 -0500

    Update, Ready for new Training

--- train.py
+++ train.py
@@ -1,8 +1,13 @@
 from ultralytics import YOLO
 
 #training on last from now one
-#model = YOLO('yolo11n-seg.pt')  # load a pretrained model (recommended for training)
+#model = YOLO('yolo11n-seg.pt')  # Model is previously used, faster + less accurate
 
-model = YOLO('D:/dev/WildFire-Detection/10_epoch_weights/last.pt')
+model = YOLO('yolo11m-seg.pt')  # Model is new, Slow + more accurate
 
-results = model.train(data="config.yaml", epochs=30, imgsz=(640, 360))
+#model = YOLO('D:/dev/WildFire-Detection/yolo11n')
+
+#mask_ratio is 1/6, shrinks our mask by the same factor the img is being resized to
+#batch increased from 16 -> 32
+#optimizer SGD = Stochastic Gradient Descent (mini batch)
+results = model.train(data="config.yaml", epochs=5, imgsz=(640, 360), mask_ratio=6, batch=32, optimizers='SGD') 
